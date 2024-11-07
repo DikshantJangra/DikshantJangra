ca3843a58243a35420038bebeb42b36287b27ed3
DikshantJangra <dikshantjangra1@gmail.com>
2024-11-07 15:44:18 +0530

    Shery Added

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ gsap.from(".mainCenter .makes", {
     duration: 1.5,
 });
 
-Shery.makeMagnet("#techStack .stackBox .br .frontend i" /* Element to target.*/, {
+Shery.makeMagnet("i, ion-icon, p, .ri-blender-fill, h2, h1, img, video" /* Element to target.*/, {
     //Parameters are optional.
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
@@ -20,30 +20,35 @@ Shery.makeMagnet("#techStack .stackBox .br .frontend i" /* Element to target.*/,
   Shery.mouseFollower({
     //Parameters are optional.
     skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
-
-  Shery.textAnimate("#techStack .techStackHead h2" /* Element to target.*/, {
+  Shery.textAnimate("#projects .projectContent p" /* Element to target.*/, {
     //Parameters are optional.
     style: 1,
     y: 10,
     delay: 0.1,
-    duration: 2,
+    duration: 0.1,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     multiplier: 0.1,
   });
 
-  Shery.imageEffect("", {
-    style: 2, //Select Style
-    debug: true, // Debug Panel
-    config: {
-      /* Config made from debug panel */
-    },
-    preset: "./presets/wigglewobble.json",
+  Shery.textAnimate("#main nav h1" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.2,
+    duration: 1,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
   });
-
-  Shery.hoverWithMediaCircle(".hover-target" /* Element to target.*/, {
-    images: ["image1.jpg", "image2.jpg", "image3.jpg"] /*OR*/,
-    //videos: ["video1.mp4", "video2.mp4"],
+   
+  Shery.textAnimate("#techStack h2, #projects h2, #connectWithMe h2" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 0.1,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
   });
